9877399df9ce763f981b0fc610946a34c4aea193
Rafamajano <rmajanochavarria@byupathway.edu>
2025-07-05 16:01:14 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+
+        int squared = SquareNumber(favoriteNumber);
+
+        DisplayResult(userName, squared);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+    }
+}
