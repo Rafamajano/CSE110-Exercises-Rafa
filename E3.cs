9fdadfe62850dc4c3d955f610ed53dfecb8f899e
Rafamajano <rmajanochavarria@byupathway.edu>
2025-07-05 16:06:24 -0600

    Add files via upload

---
+++ E3.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        Console.WriteLine("Guess My Number Game!");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    }
+}
